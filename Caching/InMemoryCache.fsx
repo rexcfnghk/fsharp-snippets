2323c4b528985bc6422e072f7e847e1d7739b106
Rex Ng <rexcfnghk@gmail.com>
2016-08-07 19:47:33 +0800

    Add a Get method to ICache

--- Caching/InMemoryCache.fsx
+++ Caching/InMemoryCache.fsx
@@ -3,17 +3,21 @@ open System.Collections.Concurrent
 
 type ICache<'TKey, 'TValue> =
     abstract TryGet : 'TKey -> 'TValue option
+    abstract Get : (unit -> 'TValue) -> 'TKey -> 'TValue
     abstract Set : TimeSpan -> 'TKey -> 'TValue -> unit
 
 type InMemoryCache<'TKey, 'TValue> (concurrentDict: ConcurrentDictionary<'TKey, ('TValue * DateTimeOffset)>) =
-    let dict = concurrentDict
     interface ICache<'TKey, 'TValue> with
         member this.TryGet key =
-            match dict.TryGetValue key with
+            match concurrentDict.TryGetValue key with
             | true, (o, expiryTimestamp) when DateTimeOffset.UtcNow <= expiryTimestamp -> Some o 
             | _ -> None 
+        member this.Get getter key =
+            match (this :> ICache<'TKey, 'TValue>).TryGet key with
+            | Some o -> o 
+            | None -> getter ()
         member this.Set expiry key value =
-            dict.[key] <- (value, DateTimeOffset.UtcNow + expiry)
+            concurrentDict.[key] <- (value, DateTimeOffset.UtcNow + expiry)
 
 let testCache = 
     InMemoryCache 
